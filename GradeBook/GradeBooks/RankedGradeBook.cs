e7ac67a20564308eb006e3f8f692e6f7def268ae
brendan albert <brendan.albert@neudesic.com>
2020-12-16 22:36:40 -0800

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,29 @@ namespace GradeBook.GradeBooks
             this.Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if ( this.Students.Count < 5 )
